e8fd10696ee7bf0955870ae2002fc478343aa306
Lievaron17 <102638804+Lievaron17@users.noreply.github.com>
2022-04-24 19:04:53 +1000

    try to get data from API and show it to webpage

--- client/src/components/AccelerometerDetail.jsx
+++ client/src/components/AccelerometerDetail.jsx
@@ -6,16 +6,7 @@ import {
   import { useHistory } from 'react-router-dom';
   import { useLocation } from "react-router-dom";
   import { useEffect } from "react";
-
-
-
-  function callApi(acc_id){
-    const id = acc_id;
-    
-    fetch(`http://localhost:3000/getirt/${id}`, {method: 'GET'})
-    .then(response => response.json())
-    .then(data => 
-      {this.setState({ accelerometer: data})});
+import { render } from '@testing-library/react';
 
       //console.log(accelerometer);
 
@@ -30,23 +21,41 @@ import {
   
     //use this reference : https://mocki.io/call-a-rest-api-in-react
   
-  }
-  
   export default function AccelerometerS1111 () {
-      const classes = useStyles();
-      const location = useLocation();
+    this.state ={
+      loading: true,
+      accelerometer: null,
+    };
+    const classes = useStyles();
+    const location = useLocation();
 
       callApi(location.state.detail); 
 
-  
+    async function callApi(acc_id){
+        const id = acc_id;
+        const url = "http://localhost:3000/getirt/${id}";
+        const response = await fetch(url, {method: 'GET'});
+        const data =  await response.json();
+        this.setState({ accelerometer: data, loading: false});
+        console.log(data)
+        }
     
-    return (
-
-      <h1> hello </h1>
-
-  
-    );
-  };
+      return (
+        <div> 
+          {this.state.loading || !this.state.accelerometer ? (
+          <div> loading...</div>
+          ) : (
+          <div>
+            <div>{this.state.accelerometer.sensor_number}</div>
+            <div>{this.state.accelerometer.vehicle_id}</div>
+            <div>{this.state.accelerometer.manufacture_date}</div>
+            <div>{this.state.accelerometer.calibration_date}</div>
+            <div>{this.state.accelerometer.installation_date}</div>
+            <div>{this.state.accelerometer.status}</div>
+          </div>)}
+        </div>
+        );
+  }
   
   // styles
   const useStyles = makeStyles((theme) => ({
